7abe4af8680e3f0ea77a37adbb44b04b0601ccb8
user <cool.rainbow2012@yandex.ru>
2019-05-02 16:01:14 +0300

    init

---
+++ app/src/main/java/root/iv/cocomoapp/MainActivity.java
@@ -0,0 +1,13 @@
+package root.iv.cocomoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
